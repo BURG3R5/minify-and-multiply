da74e75bfbc39b55fbc902f07b4bc8361f521ab9
NanoNish <nishkarsh1206@gmail.com>
2023-01-21 00:19:20 +0530

    feat: Declare and initialize globals.

---
+++ src/game/globals.ts
@@ -0,0 +1,42 @@
+var money: number;
+var size: number;
+var minimumSize: number;
+
+var numberSalesmen: number;
+var numberEngineers: number;
+var numberResearchers: number;
+
+var TRUCK_VOLUME: number;
+var UNIT_COST: number;
+var TRIM_PERCENT: number;
+var RESEARCH_MULTIPLIER: number;
+
+var SALESMAN_COST: number;
+var ENGINEER_COST: number;
+var RESEARCHER_COST: number;
+var TRIM_COST: number;
+var RESEARCH_COST: number;
+
+var emptyLine: string;
+var aboveLine: string;
+var dashedLine: string;
+var belowLine: string;
+
+var titleLine: string;
+var instructionLine: string;
+
+var sellRenderLine: string;
+var trimRenderLine: string;
+var researchRenderLine: string;
+
+var sellTimer: number;
+var trimTimer: number;
+var researchTimer: number;
+
+var sellReduceTime: number;
+var trimReduceTime: number;
+var researchReduceTime: number;
+
+var sellMaxTime: number;
+var trimMaxTime: number;
+var researchMaxTime: number;
---
+++ src/game/init.ts
@@ -0,0 +1,43 @@
+export default function initVariables() {
+  globalThis.money = 1;
+  globalThis.size = 1;
+  globalThis.minimumSize = 0.95;
+
+  globalThis.numberSalesmen = 0;
+  globalThis.numberEngineers = 0;
+  globalThis.numberResearchers = 0;
+
+  globalThis.TRUCK_VOLUME = 10;
+  globalThis.UNIT_COST = 0.1;
+  globalThis.TRIM_PERCENT = 0.05;
+  globalThis.RESEARCH_MULTIPLIER = 0.95;
+
+  globalThis.SALESMAN_COST = 25;
+  globalThis.ENGINEER_COST = 100;
+  globalThis.RESEARCHER_COST = 1000;
+  globalThis.TRIM_COST = 10;
+  globalThis.RESEARCH_COST = 100;
+
+  globalThis.sellMaxTime = globalThis.sellTimer = 25;
+  globalThis.trimMaxTime = globalThis.trimTimer = 50;
+  globalThis.researchMaxTime = globalThis.researchTimer = 100;
+
+  globalThis.sellReduceTime = 1;
+  globalThis.trimReduceTime = 1;
+  globalThis.researchReduceTime = 1;
+
+  globalThis.emptyLine =
+    "│                              │                              │                              │\n";
+  globalThis.aboveLine =
+    "╭──────────────────────────────┬──────────────────────────────┬──────────────────────────────╮\n";
+  globalThis.dashedLine =
+    "├──────────────────────────────┼──────────────────────────────┼──────────────────────────────┤\n";
+  globalThis.belowLine =
+    "╰──────────────────────────────┴──────────────────────────────┴──────────────────────────────╯\n";
+
+  //Title ASCII art from https://fsymbols.com/text-art/ => enter "VSC_GAME"
+  globalThis.titleLine =
+    "\n██╗░░░██╗░██████╗░█████╗░░░░░░░░██████╗░░█████╗░███╗░░░███╗███████╗\n██║░░░██║██╔════╝██╔══██╗░░░░░░██╔════╝░██╔══██╗████╗░████║██╔════╝\n╚██╗░██╔╝╚█████╗░██║░░╚═╝█████╗██║░░██╗░███████║██╔████╔██║█████╗░░\n░╚████╔╝░░╚═══██╗██║░░██╗╚════╝██║░░╚██╗██╔══██║██║╚██╔╝██║██╔══╝░░\n░░╚██╔╝░░██████╔╝╚█████╔╝░░░░░░╚██████╔╝██║░░██║██║░╚═╝░██║███████╗\n░░░╚═╝░░░╚═════╝░░╚════╝░░░░░░░░╚═════╝░╚═╝░░╚═╝╚═╝░░░░░╚═╝╚══════╝\n";
+  globalThis.instructionLine =
+    "Press '1' to sell, '2' to trim, '3' to research\n";
+}
